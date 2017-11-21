8d537219ea55d8ffbab0584c98c615b130e6506f
Ben Hall <ben@benhall.me.uk>
2017-11-21 11:16:08 +0000

    Run all in the foreground to see what's happening

--- scenarios/getting-started/foreground.sh
+++ scenarios/getting-started/foreground.sh
@@ -1,1 +1,3 @@
+curl -LO https://github.com/RedHat-Middleware-Workshops/modernize-apps-labs/archive/master.zip
+unzip master.zip
 cd modernize-apps-labs-master/
--- scenarios/moving-existing-apps/run-init.sh
+++
@@ -1,2 +0,0 @@
-curl -LO https://github.com/RedHat-Middleware-Workshops/modernize-apps-labs/archive/master.zip
-unzip master.zip
