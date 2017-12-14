b9bb9f051bf1d94c10f18d47456a11bb73da978e
jamesfalkner <schtool@gmail.com>
2017-12-14 14:07:14 -0500

    update path

--- scenarios/moving-existing-apps/run-init.sh
+++ scenarios/moving-existing-apps/run-init.sh
@@ -3,7 +3,7 @@ ssh root@host01 "mkdir -p /root/projects && cd /root/projects && git clone https
 ssh root@host01 "setenforce 0 || true"
 ssh root@host01 "mkdir /tmp/pv-01 /tmp/pv-02 /tmp/pv-03"
 ssh root@host01 "chmod 0777 /tmp/pv-01 /tmp/pv-02 /tmp/pv-03"
-ssh root@host01 "oc create -f volumes.json"
+ssh root@host01 "oc create -f /root/volumes.json"
 #ssh root@host01 "docker pull schtool/foo:latest"
 #ssh root@host01 "docker pull schtool/foo2:latest"
 ssh root@host01 'for i in {1..200}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done'
