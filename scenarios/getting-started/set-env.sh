574e2fdffdfdd11e6f6a114f12ebf111b2eae5b5
jamesfalkner <schtool@gmail.com>
2017-12-19 17:11:48 -0500

    temp pull code

--- scenarios/getting-started/set-env.sh
+++ scenarios/getting-started/set-env.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-
 ${HOME}/.launch.sh
-cd ${HOME}/projects
-git clone https://github.com/RedHat-Middleware-Workshops/modernize-apps-labs
-cd modernize-apps-labs/monolith
+cd ${HOME}/projects/modernize-apps-labs/monolith
