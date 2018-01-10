ec272e76fd4c42d55e2e22adbbd0d98d388e2948
jamesfalkner <schtool@gmail.com>
2018-01-10 16:15:30 -0500

    remove all-in-one testing scenario

--- scenarios/all-in-one/run-init.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-ssh root@host01 "oc import-image jenkins:v3.7 --from='registry.access.redhat.com/openshift3/jenkins-2-rhel7:v3.7' --confirm -n openshift"
-ssh root@host01 "oc export template jenkins-persistent -n openshift -o json | sed 's/jenkins:latest/jenkins:v3.7/g' | oc replace -f - -n openshift"
-ssh root@host01 "oc export template jenkins-ephemeral -n openshift -o json | sed 's/jenkins:latest/jenkins:v3.7/g' | oc replace -f - -n openshift"
-
-# Install templates
-ssh root@host01 "oc create -n openshift -f https://raw.githubusercontent.com/RedHat-Middleware-Workshops/modernize-apps-labs/master/monolith/src/main/openshift/template-binary.json"
-ssh root@host01 "oc create -n openshift -f https://raw.githubusercontent.com/RedHat-Middleware-Workshops/modernize-apps-labs/master/monolith/src/main/openshift/template-prod.json"
