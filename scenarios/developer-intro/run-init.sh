3c04da60f71d9017ca1649dee0fcbc80beb58b16
Thomas Qvarnström <tqvarnst@redhat.com>
2018-01-13 23:18:07 +0100

    Updated the .init.sh script

--- scenarios/developer-intro/run-init.sh
+++ scenarios/developer-intro/run-init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ssh root@host01 "bash <(curl -sL https://gist.githubusercontent.com/tqvarnst/b5dc3fccb469c067cef8ead32739e60d/raw/ae8cc3d51232f6490e1f78c3b14e8b0b9e139294/.init.sh)"
+ssh root@host01 "bash <(curl -sL https://gist.githubusercontent.com/tqvarnst/b5dc3fccb469c067cef8ead32739e60d/raw/b5b391ced0a14e39c6721f799b3b4b740a3a86a9/.init.sh)"
