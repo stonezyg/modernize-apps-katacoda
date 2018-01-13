870c92c5e2559330c6d02d3532f254af2796bbc6
Thomas Qvarnström <tqvarnst@redhat.com>
2018-01-13 22:44:44 +0100

    minor fixes

--- scenarios/reactive-microservices/run-init.sh
+++ scenarios/reactive-microservices/run-init.sh
@@ -1,33 +1,34 @@
 #!/usr/bin/env bash
-set +x
+
 ssh -T root@host01 <<'EOSSH'
 echo "Running init commands" | tee ${HOME}/.init.log
-if [ "$(oc whoami)" -eq "system:admin"]; then
-  echo "Since user is system:admin" | tee ${HOME}/.init.log
-  MASTER_EXTERNAL_URL=$(oc get route/docker-registry -n default | grep -v NAME | awk '{print $2}' | sed 's/docker\-registry\-default\.//')
+if [ "$(oc whoami)" == "system:admin" ]; then
+  echo "Since user is system:admin" | tee -a ${HOME}/.init.log
+  
   oc adm policy add-role-to-user system:image-puller system:anonymous
   oc adm policy add-cluster-role-to-user cluster-admin admin
   oc import-image jenkins:v3.7 --from='registry.access.redhat.com/openshift3/jenkins-2-rhel7:v3.7' --confirm -n openshift
   oc export template jenkins-persistent -n openshift -o json | sed 's/jenkins:latest/jenkins:v3.7/g' | oc replace -f - -n openshift
   oc export template jenkins-ephemeral -n openshift -o json | sed 's/jenkins:latest/jenkins:v3.7/g' | oc replace -f - -n openshift
 
-  echo "Installing template" | tee ${HOME}/.init.log
+  echo "Installing template" | tee -a ${HOME}/.init.log
   oc create -n openshift -f https://raw.githubusercontent.com/RedHat-Middleware-Workshops/modernize-apps-labs/master/monolith/src/main/openshift/template-binary.json
   oc create -n openshift -f https://raw.githubusercontent.com/RedHat-Middleware-Workshops/modernize-apps-labs/master/monolith/src/main/openshift/template-prod.json
 
-  echo "Disable namespace ownership for router" | tee ${HOME}/.init.log
+  echo "Disable namespace ownership for router" | tee -a ${HOME}/.init.log
   oc env dc/router ROUTER_DISABLE_NAMESPACE_OWNERSHIP_CHECK=true -n default
 
-  echo "Installing tree" | tee ${HOME}/.init.log
+  echo "Installing tree" | tee -a ${HOME}/.init.log
   yum install tree -y
-  
+
+  MASTER_EXTERNAL_URL=$(oc get route/docker-registry -n default | grep -v NAME | awk '{print $2}' | sed 's/docker\-registry\-default\.//' | sed 's/\-80\-/\-8443\-/')
+  oc login $MASTER_EXTERNAL_URL -u developer -p developer --insecure-skip-tls-verify=true
+  echo "Init successeded" | tee -a ${HOME}/.init.log  
 
   docker pull registry.access.redhat.com/openshift3/jenkins-2-rhel7:v3.7
   docker pull registry.access.redhat.com/openshift3/jenkins-2-rhel7:latest
-
-  oc login ${MASTER_EXTERNAL_URL} -u developer -p developer --insecure-skip-tls-verify=true
-  echo "Init successeded" | tee ${HOME}/.init.log
-
+else
+  echo "Skipping init since user is not system:admin anymore" | tee -a ${HOME}/.init.log
 fi
 
 git --git-dir=/root/projects/.git --work-tree=/root/projects pull
