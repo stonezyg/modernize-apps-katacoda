199e9a524c017cab3473306cc9f87c3b43f9b4ca
Thomas Qvarnström <tqvarnst@redhat.com>
2018-01-13 22:22:27 +0100

    test again

--- scenarios/reactive-microservices/run-init.sh
+++ scenarios/reactive-microservices/run-init.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+set +x
 ssh -T root@host01 <<'EOSSH'
 touch ${HOME}/.tqstart
 if [ "$(oc whoami)" -eq "system:admin"]; then
   touch ${HOME}/.tqtrue
   MASTER_EXTERNAL_URL=$(oc get route/docker-registry -n default | grep -v NAME | awk '{print $2}' | sed 's/docker\-registry\-default\.//')
-  for i in {1..200}; do oc adm policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done'
-  for i in {1..200}; do oc adm policy add-cluster-role-to-user cluster-admin admin && break || sleep 1; done'
+  oc adm policy add-role-to-user system:image-puller system:anonymous
+  do oc adm policy add-cluster-role-to-user cluster-admin admin
   oc import-image jenkins:v3.7 --from='registry.access.redhat.com/openshift3/jenkins-2-rhel7:v3.7' --confirm -n openshift
   oc export template jenkins-persistent -n openshift -o json | sed 's/jenkins:latest/jenkins:v3.7/g' | oc replace -f - -n openshift
   oc export template jenkins-ephemeral -n openshift -o json | sed 's/jenkins:latest/jenkins:v3.7/g' | oc replace -f - -n openshift
