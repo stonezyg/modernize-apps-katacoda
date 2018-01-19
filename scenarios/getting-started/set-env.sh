72161eb63abb49dfc4a98ddf06dc69d7abe5fac3
Thomas Qvarnström <tqvarnst@redhat.com>
2018-01-19 11:54:59 +0800

    test

--- scenarios/getting-started/set-env.sh
+++ scenarios/getting-started/set-env.sh
@@ -1,6 +1,6 @@
 stty_orig=`stty -g`
 stty -echo
 cd ${HOME}/projects
-echo "Your ip-address for this cluster is $(hostname -i), please record this so that the instructor can reboot you machine if needed."
+echo "Your ip-address for this cluster is $(hostname -i), please record this so that the instructor can reboot you machine if needed." >&2
 stty $stty_orig
 
