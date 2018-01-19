2a5dd791d486fad98a36aae444bf54920da37327
Thomas Qvarnström <tqvarnst@redhat.com>
2018-01-19 12:02:57 +0800

    test

--- scenarios/getting-started/set-env.sh
+++ scenarios/getting-started/set-env.sh
@@ -1,4 +1,4 @@
 cd ${HOME}/projects
-clear; echo "Your ip-address for this cluster is $(hostname -i), please record this so that the instructor can reboot you machine if needed."
+clear; echo "\033[0;32Your ip-address for this cluster is $(hostname -i), please record this so that the instructor can reboot you machine if needed.\033[0m"
 
 
