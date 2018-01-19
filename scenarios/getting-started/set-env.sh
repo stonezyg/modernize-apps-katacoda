fdbb6c1af0bf012088e0814278ad1827e82f1d25
Thomas Qvarnström <tqvarnst@redhat.com>
2018-01-19 12:15:18 +0800

    test

--- scenarios/getting-started/set-env.sh
+++ scenarios/getting-started/set-env.sh
@@ -1,7 +1,4 @@
 cd ${HOME}/projects
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
 clear; echo; echo -e "\033[0;32mYour ip-address for this cluster is $(hostname -i), please record this so that the instructor can reboot your machine if needed.\033[0m";echo
 
 
